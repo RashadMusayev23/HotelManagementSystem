bd0ce95ecd6fb45eb8a72fc21aee61aaded2d8ac
meteorgas <musayevresad23@gmail.com>
2024-12-18 21:11:27 +0300

    Small Change

--- src/db/DBUtil.java
+++ src/db/DBUtil.java
@@ -7,7 +7,9 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/hotel_management";
     private static final String USER = "root";
-    private static final String PASSWORD = "Emad$2013";
+    private static final String PASSWORD = "mazeppa1881";
+    //mazeppa1881
+    //Emad$2013
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
